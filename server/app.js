6fcbd7de1f97dac3952f0db4d30da94b8ce89534
panzhilov <panzhilov@gmail.com>
2022-07-21 11:49:15 +0300

    initialize mongoose

--- server/app.js
+++ server/app.js
@@ -1,8 +1,26 @@
-const express = require('express')
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app = express();
+const cors = require("./middlewares/cors");
 
+function start() {
+  try {
+    const database = mongoose.connect(
+      "mongodb://localhost:27017/photo-fantasy"
+    );
+    console.log("DB Ready");
+  } catch (error) {
+    console.log("Have problem with connecting to database");
+  }
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
-})
+  const app = express();
+  
+  app.use(express.json());
+  app.use(cors());
+
+  app.listen(3000, () => {
+    console.log("Server listening on port 3000");
+  });
+}
+
+start();
---
+++ server/middlewares/cors.js
@@ -0,0 +1,12 @@
+module.exports = () => (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "HEAD, GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+};
